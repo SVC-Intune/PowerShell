3e8a13835da1c0b31e908a324638edc4dddc0a98
onion-Skagit <orion.liedtke@skagit.edu>
2026-05-06 11:23:08 -0700

    timeout

---
+++ packaging/timeout.ps1
@@ -0,0 +1,51 @@
+Add-Type @'
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace PInvoke.Win32 {
+    public static class UserInput {
+        [DllImport("user32.dll", SetLastError=false)]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LASTINPUTINFO {
+            public uint cbSize;
+            public int dwTime;
+        }
+        public static DateTime LastInput {
+            get {
+                DateTime bootTime = DateTime.UtcNow.AddMilliseconds(-Environment.TickCount);
+                DateTime lastInput = bootTime.AddMilliseconds(LastInputTicks);
+                return lastInput;
+            }
+        }
+
+        public static TimeSpan IdleTime {
+            get {
+                return DateTime.UtcNow.Subtract(LastInput);
+            }
+        }
+
+        public static int LastInputTicks {
+            get {
+                LASTINPUTINFO lii = new LASTINPUTINFO();
+                lii.cbSize = (uint)Marshal.SizeOf(typeof(LASTINPUTINFO));
+                GetLastInputInfo(ref lii);
+                return lii.dwTime;
+            }
+        }
+    }
+}
+'@
+
+
+while ($true) {
+    $Last = [PInvoke.Win32.UserInput]::LastInput
+    $Idle = [PInvoke.Win32.UserInput]::IdleTime
+    $LastStr = $Last.ToLocalTime().ToString("MM/dd/yyyy hh:mm tt")
+    Write-Host ("   Idle for " + $Idle.Hours + " hours, " + $Idle.Minutes + " minutes, " + $Idle.Seconds + " seconds.")
+    Start-Sleep -Seconds (1)
+    if ($Idle.Minutes -ge 4){
+        shutdown /l
+    }
+}
